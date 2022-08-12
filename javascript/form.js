ce8434f13dc294ec3d96b966760ab8ad70bf3ac1
Seba <sebadalessandro@hotmail.com>
2022-08-12 12:51:04 -0300

    :sparkles:fix: padding of the nav

--- javascript/form.js
+++ javascript/form.js
@@ -1,15 +1,11 @@
 const inputName = document.querySelector('#inputName')
 const inputEmail = document.querySelector('#inputEmail')
 const formMensaggeId = document.querySelector('#formMensaggeId')
-
 const formBtn = document.querySelector('#formBtn')
 
 formBtn.addEventListener('click', (e)=>{
-    e.preventDefault()
+    e.preventDefault() //IMPORTANTE: al utilizar este metodo no se ven los pattern generados en el HTML
     console.log('Name: ', inputName.value)
     console.log('Email: ', inputEmail.value)
     console.log('Mensagge: ', formMensaggeId.value)
-    inputName.value = '';
-    inputEmail.value = '';
-    formMensaggeId.value = '';
 })
